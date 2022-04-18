e1782c951d1d9aa10e286103922e40de30ee76e7
Musa Rafik <31826652+musarafik@users.noreply.github.com>
2022-04-17 23:45:01 -0500

    added styling for header componnet

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import {Link, Outlet} from "react-router-dom";
+import "./header.css";
 
 function Header() {
     return (
         <div>
             <nav>
-                <Link to="/">Home</Link>
+                <Link className="link" style={{marginRight: "10%"}} to="/">Home</Link>
                 <Link to="all">All Questions</Link>
             </nav>
             <Outlet/>
